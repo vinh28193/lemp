17ff765c3ffaa40e395ff34ac5ff35e131465292
Vinh <vinh28193@gmail.com>
2016-06-02 16:18:26 +0700

    init API

---
+++ api/config/main.php
@@ -0,0 +1,36 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),    
+    'bootstrap' => ['log'],
+    'modules' => [
+        'v1' => [
+            'basePath' => '@app/versions/v1',
+            'class' => 'api\versions\v1\Version1'
+        ]
+    ],
+    'components' => [        
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',   
+                ]
+            ],        
+        ]
+    ],
+    'params' => $params,
+];
+
+
+
