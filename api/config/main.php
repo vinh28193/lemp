6c4a24b7d34abaea581ea6b1db0a9fed5ce70a82
Vinh <vinh28193@gmail.com>
2016-06-03 10:49:23 +0700

    interface

--- api/config/main.php
+++ api/config/main.php
@@ -17,17 +17,38 @@ return [
             'class' => 'api\versions\v1\Version1'
         ]
     ],
-    'components' => [        
+    'components' => [
+        'response' => [
+            'format' => yii\web\Response::FORMAT_JSON,
+            'charset' => 'UTF-8',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'request' => [
+            'class' => '\yii\web\Request',
+            'enableCookieValidation' => false,
+            'parsers' => [
+                'application/json' => yii\web\JsonParser::className(),
+            ],
+        ],
         'urlManager' => [
             'enablePrettyUrl' => true,
             'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
-                [
-                    'class' => 'yii\rest\UrlRule',   
-                ]
-            ],        
-        ]
+                ['class' => 'yii\rest\UrlRule', 'controller' => ['v1/post', 'v1/comment', 'v2/post']],
+                'OPTIONS v1/user/login' => 'v1/user/login',
+                'POST v1/user/login' => 'v1/user/login',
+                'POST v2/user/login' => 'v2/user/login',
+                'OPTIONS v2/user/login' => 'v2/user/login',
+            ],
+        ],
     ],
     'params' => $params,
 ];
---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+#DEMO
+http://angularyii2.github.io/
+
+# [Wiki (FAQ)](https://github.com/AngularYii2/angularyii2.github.io/wiki)
